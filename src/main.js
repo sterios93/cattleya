90c0df645c3732afc387d1480d30517b79f1d743
stery <sterios.taskudis@gmail.com>
2018-10-21 16:40:15 +0300

    fix: include Bootstrap 4 and FontAwesome into the project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+// Font awesome icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// Font awesome icons config
+library.add(faBars);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
