57955b747ede336316db143ebdfdb15842c24b70
stery <sterios.taskudis@gmail.com>
2018-10-21 16:41:25 +0300

    fix: Create Navigation and Header Components

---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+        <!-- Page Header -->
+        <header class="masthead">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-10 mx-auto">
+                        <div class="site-heading text-center mt-0 text-white">
+                            <h1>Clean Blog</h1>
+                            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+</template>
+
+<style scoped lang="stylus">
+    .masthead
+        background: url("../assets/header-main.jpg")
+        position: relative
+        background-size: cover
+        background-position: center bottom
+        background-repeat: no-repeat
+        .overlay
+            position: absolute
+            top: 0
+            left: 0
+            height: 100%
+            width: 100%
+            background-color: #212529
+            opacity: 0.5
+
+    .site-heading
+        padding: 200px 0 150px
+        h1
+            font-size: 50px
+        .subheading
+            font-size: 24px
+            font-weight: 300
+            line-height: 1.1
+            margin: 10px 0 0
+            font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif
+
+    @media only screen and (min-width: 768px)
+        .site-heading
+            padding: 200px 0
+            h1
+                font-size: 80px
+
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- Navigation -->
+    <div>
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                <div class="container">
+                    <a class="navbar-brand text-white" href="#">Cattleya</a>
+                    <button class="navbar-toggler"
+                            type="button" data-toggle="collapse"
+                            data-target="#hamburgerMenu" aria-controls="hamburgerMenu"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <font-awesome-icon icon="bars" class="text-white"/>
+                    </button>
+                    <div class="collapse navbar-collapse" id="hamburgerMenu">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#">Sample Post</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+</template>
+
+<style>
+
+</style>
