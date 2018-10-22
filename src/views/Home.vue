36242f585dec50cae85bd6389af5ced72d4971bd
stery <sterios.taskudis@gmail.com>
2018-10-22 23:00:59 +0300

    fix: Include the Article component into the Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="home">
     <Header></Header>
+    <main>
+      <Article v-bind:pictureLeft="true"></Article>
+      <!--<Article v-bind:pictureLeft="false"></Article>-->
+      <!--<Article v-bind:pictureLeft="true"></Article>-->
+    </main>
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue';
+import Article from '../components/Article.vue';
 
 export default {
   name: 'home',
   components: {
     Header,
+    Article,
   },
 };
 </script>
