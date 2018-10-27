ae06987cb318bc8248050523c56698e4b709b54e
stery <sterios.taskudis@gmail.com>
2018-10-27 22:02:59 +0300

    fix: create Map component and add it into the Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
       <Article v-bind:pictureLeft="true"></Article>
       <Article v-bind:pictureLeft="false"></Article>
       <Article v-bind:pictureLeft="true"></Article>
+      <google-map></google-map>
     </main>
   </div>
 </template>
@@ -12,12 +13,14 @@
 <script>
 import Header from '../components/Header.vue';
 import Article from '../components/Article.vue';
+import GoogleMap from '../components/Map.vue';
 
 export default {
   name: 'home',
   components: {
     Header,
     Article,
+    GoogleMap,
   },
 };
 </script>
