ae0205d20d3f204cd194df96243f9c4e2fd30aa4
stery <sterios.taskudis@gmail.com>
2018-10-27 22:02:14 +0300

    fix: Tell Vue to use vue-google-maps and set the api key

--- src/main.js
+++ src/main.js
@@ -4,11 +4,20 @@ import 'bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import * as VueGoogleMaps from 'vue2-google-maps';
+
 
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyB4zoOi_AiJ6y_c459Aica5WXmnQc31xq4',
+  },
+});
 
 // Font awesome icons config
 library.add(faBars);
