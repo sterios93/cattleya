4add918a9be0cf8f8bb54bd0999ee423353eab0b
stery <sterios.taskudis@gmail.com>
2018-10-28 10:23:21 +0200

    fix: Create Footer component and add it to the App view

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <Navigation></Navigation>
     <router-view/>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Navigation from './components/Navigation.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   components: {
     Navigation,
+    Footer,
   },
 };
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="footer pt-3 pb-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-8 d-flex justify-content-center justify-content-lg-start">
+                    <nav class="nav">
+                        <a class="nav-link text-white" href="#">Apartments</a>
+                        <a class="nav-link text-white" href="#">Environment</a>
+                        <a class="nav-link text-white" href="#">The Project</a>
+                        <a class="nav-link text-white" href="#">About Us</a>
+                        <a class="nav-link text-white" href="#">Contact Us</a>
+                    </nav>
+                </div>
+                <div class="col-12 col-lg-4 d-flex
+                justify-content-center justify-content-lg-end align-items-center">
+                    <p class="m-0 text-white">Copyright 2018 - Created by Put our name here.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="stylus">
+    .footer
+        background #2c2c2c;
+</style>
