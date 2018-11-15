0ed08ec6b555669e63222d9d7b95b829ea4d70fe
stery <sterios.taskudis@gmail.com>
2018-11-15 22:54:17 +0200

    feat: Create Apartments view component

---
+++ src/views/Apartments.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <apartment-container></apartment-container>
+    </div>
+</template>
+
+<script>
+import ApartmentContainer from '../components/Apartment/ApartmentContainer.vue';
+
+export default {
+  components: {
+    ApartmentContainer,
+  },
+};
+</script>
