313c6f00e78dbce6d510ef64f8b4a6f2a583d07e
stery <sterios.taskudis@gmail.com>
2018-11-15 22:38:15 +0200

    fix: add apartments route , apartmentInfo path

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Apartments from './views/Apartments.vue';
+import ApartmentInfo from './views/ApartmentInfo.vue';
 
 Vue.use(Router);
 
@@ -12,6 +14,17 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/apartments',
+      name: 'apartments',
+      component: Apartments,
+    },
+    {
+      path: '/apartments/:id',
+      props: true,
+      name: 'apartmentInfo',
+      component: ApartmentInfo,
     },
   ],
 });
