0c9fb487bbe346a54cc11a7b269b8eb5ed51c50e
stery <sterios.taskudis@gmail.com>
2018-11-24 19:05:39 +0200

    fix: create apartment card component.

---
+++ src/components/Apartment/ApartmentCard.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="col-12 col-md-6 mt-5 mt-md-0" @click="checkApartment" id="a1">
+        <div class="col-12 col-md-10 apartmentCard p-0">
+            <div class="position-relative">
+                <img class="w-100" src="../../assets/half.png" alt="Card image cap">
+                <span class="position-absolute bg-info text-white p-1 mt-3 mr-3
+                 sellLabel font-weight-bold"
+                      style="top: 0; right: 0;">
+                    FOR SELL</span>
+                <span class="position-absolute font-weight-bold ml-2 priceLabel"
+                      style="bottom: 0; left: 0;">$ 150 000</span>
+            </div>
+            <div class="descriptionBg p-2">
+                <div>
+                    <p>Modern and luxury apartment</p>
+                    <p><font-awesome-icon icon="map-marker"/> Sofia - Student City</p>
+                    <div class="row">
+                        <div class="col-6 col-xl-4">
+                            <p>150 sq.m.</p>
+                        </div>
+                        <div class="col-6 col-xl-4">
+                            <p>3 Bedrooms</p>
+                        </div>
+                        <div class="col-6 col-xl-4">
+                            <p>2 Bathrooms</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: '',
+    };
+  },
+  methods: {
+    checkApartment() {
+      const apartmentId = 1;
+      this.$router.push({ path: `/apartments/${apartmentId}`, params: { apartmentId } });
+    },
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+    .priceLabel
+        font-size 20px;
+    .sellLabel
+        font-size: 12px;
+    .descriptionBg
+        background: #f7f7f7;
+
+    .apartmentCard:hover {
+        cursor pointer
+        transform: scale(1);
+        -webkit-box-shadow: -1px -5px 19px 2px rgba(219,219,219,1);
+        -moz-box-shadow: -1px -5px 19px 2px rgba(219,219,219,1);
+        box-shadow: -1px -5px 19px 2px rgba(219,219,219,1);
+    }
+</style>
+
--- src/components/Apartment/ApartmentContainer.vue
+++ src/components/Apartment/ApartmentContainer.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="container mt-5 mb-5">
-        <div class="row mt-5">
-            <div class="col-2">
+    <div class="container mt-lg-5 mb-5">
+        <div class="row mt-lg-5 d-flex flex-column flex-md-row">
+            <div class="col-12 col-md-2 d-none d-md-block">
                 <p>FLOOR</p>
-                <nav class="nav flex-column">
+                <nav class="nav flex-row flex-md-column">
                     <a class="nav-link floor-numbers text-left font-weight-bold active"
                        @click="changeFloor" data-floor="f6" href="#">6</a>
                     <a class="nav-link floor-numbers text-left font-weight-bold"
@@ -20,14 +20,37 @@
                        @click="changeFloor" data-floor="fm1" href="#">-1</a>
                 </nav>
             </div>
-            <div class="col-10">
-                <Floor6 v-if="floors.f6"></Floor6>
-                <Floor5 v-if="floors.f5"></Floor5>
-                <Floor4 v-if="floors.f4"></Floor4>
-                <Floor3 v-if="floors.f3"></Floor3>
-                <Floor2 v-if="floors.f2"></Floor2>
-                <Floor1 v-if="floors.f1"></Floor1>
-                <FloorM1 v-if="floors.fm1"></FloorM1>
+            <div class="d-flex d-md-none row m-0">
+                <div class="col-6">
+                    <div class="dropdown col-10">
+                        <button class="btn btn-lg dropdown-toggle" type="button"
+                                id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                            Select Floor
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item text-center" href="#">-1</a>
+                            <a class="dropdown-item text-center" href="#">1</a>
+                            <a class="dropdown-item text-center" href="#">2</a>
+                            <a class="dropdown-item text-center" href="#">3</a>
+                            <a class="dropdown-item text-center" href="#">4</a>
+                            <a class="dropdown-item text-center" href="#">5</a>
+                            <a class="dropdown-item text-center" href="#">6</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-6 d-flex align-items-center justify-content-end">
+                    <a href="#" class="downloadButon btn">
+                        <h5 class="m-0"><font-awesome-icon icon="download"/> Floor Plan</h5>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-10">
+                <div class="row">
+                    <ApartmentCard></ApartmentCard>
+                    <ApartmentCard></ApartmentCard>
+                </div>
             </div>
         </div>
     </div>
@@ -41,6 +64,7 @@ import Floor3 from './Floor3.vue';
 import Floor2 from './Floor2.vue';
 import Floor1 from './Floor1.vue';
 import FloorM1 from './FloorM1.vue';
+import ApartmentCard from './ApartmentCard.vue';
 
 export default {
   components: {
@@ -51,6 +75,7 @@ export default {
     Floor2,
     Floor1,
     FloorM1,
+    ApartmentCard,
   },
   data() {
     return {
@@ -99,4 +124,8 @@ export default {
         color: #9C806B;
     nav a:last-child
         padding-left 5px;
+    .downloadButon
+        background #9c806b;
+        /*padding: 11px 15px 5px 15px;*/
+        border-radius: 5px;
 </style>
