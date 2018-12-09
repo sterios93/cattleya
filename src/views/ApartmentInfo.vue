5f70dc01c202b3315a9f0f6e86919ba10515fdad
stery <sterios.taskudis@gmail.com>
2018-12-10 01:59:26 +0200

    fix: create ApartmentInfo view.

--- src/views/ApartmentInfo.vue
+++ src/views/ApartmentInfo.vue
@@ -2,7 +2,7 @@
     <div class="container mt-5 mb-5">
         <div class="d-none d-lg-block mt-5 mt-sm-0" style="height: 70px"></div>
         <div class="row">
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6 order-2 order-lg-1">
                 <!--Apartment number-->
                 <div class="row">
                     <div class="col-6 d-flex">
@@ -10,7 +10,7 @@
                     </div>
                     <div class="col-6 d-flex align-items-center justify-content-end">
                         <a href="#" class="downloadButon">
-                            <h5><font-awesome-icon icon="download"/> Download Plan</h5>
+                            <h5><font-awesome-icon icon="download"/> Свали план.</h5>
                         </a>
                     </div>
                 </div>
@@ -18,20 +18,26 @@
                 <div class="row">
                     <div class="col-12 d-flex flex-column">
                         <div>
-                            <h5 class="bronzColor font-weight-bold">Details</h5>
+                            <h5 class="bronzColor font-weight-bold">Детайли</h5>
                         </div>
                         <div class="row">
-                            <div class="col-6 col-lg-4 py-2">
-                                <font-awesome-icon icon="square"/> 157 sq.m.</div>
-                            <div class="col-6 col-lg-4 py-2">
+                            <div v-if="apartment.details.bedrooms === 1"
+                                 class="col-6 col-lg-4 py-2">
                                 <font-awesome-icon icon="bed"/>
-                                {{apartment.details.bedrooms }} Bedrooms</div>
-                            <div class="col-6 col-lg-4 py-2">
+                                {{apartment.details.bedrooms }} Спалня</div>
+                             <div v-else class="col-6 col-lg-4 py-2">
+                                <font-awesome-icon icon="bed"/>
+                                {{apartment.details.bedrooms }} Спални</div>
+                            <div v-if="apartment.details.bathrooms === 1"
+                                 class="col-6 col-lg-4 py-2">
                                 <font-awesome-icon icon="bath"/>
-                                {{ apartment.details.bathrooms}} Bathrooms</div>
+                                {{ apartment.details.bathrooms}} Баня</div>
+                            <div v-else class="col-6 col-lg-4 py-2">
+                                <font-awesome-icon icon="bath"/>
+                                {{ apartment.details.bathrooms}} Бани</div>
                             <div class="col-6 col-lg-4 py-2">
                                 <font-awesome-icon icon="building"/>
-                                Floor {{ apartment.details.floor}}/6</div>
+                                Етаж {{ apartment.details.floor}}/6</div>
                             <div class="col-6 col-lg-4 py-2">
                                 <font-awesome-icon icon="ad"/> {{apartment.details.brick}}</div>
                         </div>
@@ -41,8 +47,11 @@
                 <div class="row">
                     <div class="col-12 d-flex flex-column">
                         <div>
-                            <h5 class="pt-4 bronzColor font-weight-bold">Description</h5>
+                            <h5 class="pt-4 bronzColor font-weight-bold">Описание</h5>
                         </div>
+                        <p v-if="apartment.sold"
+                           class="font-weight-bold p-0 m-0" style="text-decoration: underline;">
+                            ПРОДАДЕН</p>
                         <p>
                             {{apartment.description}}
                         </p>
@@ -52,26 +61,27 @@
                 <div class="row">
                     <div class="col-12 d-flex flex-column">
                         <div>
-                            <h5 class="pt-3 pb-1 bronzColor font-weight-bold">Features</h5>
+                            <h5 class="pt-3 pb-1 bronzColor font-weight-bold">Допълнение</h5>
                         </div>
                         <div class="row">
                             <div class="col-6 col-lg-4 py-2">
-                                <font-awesome-icon icon="check" class="bronzColor"/> 157 sq.m.</div>
+                                <font-awesome-icon icon="check" class="bronzColor"/>
+                                {{apartment.features.controlledAccess}}</div>
                             <div class="col-6 col-lg-4 py-2">
-                                <font-awesome-icon icon="check" class="bronzColor"/> 1 Bedroom</div>
+                                <font-awesome-icon icon="check" class="bronzColor"/>
+                                {{apartment.features.elevator}}</div>
                             <div class="col-6 col-lg-4 py-2">
-                                <font-awesome-icon icon="check"
-                                                   class="bronzColor"/> 2 Bathrooms</div>
+                                <font-awesome-icon icon="check" class="bronzColor"/>
+                                {{apartment.features.parking}}</div>
                             <div class="col-6 col-lg-4 py-2">
-                                <font-awesome-icon icon="check" class="bronzColor"/> Floor 6/6</div>
-                            <div class="col-6 col-lg-4 py-2">
-                                <font-awesome-icon icon="check" class="bronzColor"/> Brick</div>
+                                <font-awesome-icon icon="check" class="bronzColor"/>
+                                {{apartment.details.balcony}}</div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6">
-                <img class="mw-100" src="../assets/half.png">
+            <div class="col-12 col-md-6 order-1 order-lg-2">
+                <img class="mw-100" :src="require(`../assets/apartments/${apartment.imageSrc}`)">
             </div>
         </div>
     </div>
