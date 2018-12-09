048e4363bc0699a581385426f5eb0f663cca0974
stery <sterios.taskudis@gmail.com>
2018-12-10 01:53:27 +0200

    fix: create About page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,37 @@
 <template>
     <div class="mt-5">
-        <div class="mt-0 mt-sm-5"></div>
-        <p>About page mock</p>
+      <Spinner></Spinner>
+        <div class="mt-0 mt-sm-5 pt-lg-5"></div>
+        <Article v-bind:pictureLeft="false" :article="about.team" :isHome="false"></Article>
+        <Article v-bind:pictureLeft="true" :article="about.ceo" :isHome="false"></Article>
+        <google-map></google-map>
     </div>
 </template>
+<script>
+import { mapGetters } from 'vuex';
+import Article from '../components/Article.vue';
+import GoogleMap from '../components/Map.vue';
+import Spinner from '../components/Spinner.vue';
+
+export default {
+  name: 'about',
+  components: {
+    Article,
+    GoogleMap,
+    Spinner,
+  },
+  data() {
+    return {
+      about: {},
+    };
+  },
+  computed: {
+    ...mapGetters([
+      'getAbout',
+    ]),
+  },
+  beforeMount() {
+    this.about = this.getAbout;
+  },
+};
+</script>
