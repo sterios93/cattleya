8a63adac3fc66054a9e1fdd43f8453464f07344d
stery <sterios.taskudis@gmail.com>
2018-12-10 23:25:36 +0200

    fix: update contact page.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,8 +1,36 @@
 <template>
     <div class="mt-5">
-        <div class="mt-0 mt-sm-5"></div>
-        <p>Contact page mock</p>
+        <div class="mt-5 mt-sm-5"></div>
+        <div class="container">
+           <div>
+               <h1 class="heading text-center">Контакти</h1>
+               <p>Офис 1: град София, ул. 'Г. С. Раковски' 183, ет. 2, ап.
+                   8, <a href="tel:0888619844">0888619844</a>,
+                   <a href="tel:029800950">02/9800950</a>,
+                   <a href="tel:029803170">9803170</a></p>
+               <p>Офис 2: град София, бул. 'България' 60В, ет. 3, офис
+                   7,ВИЛИДЖ РЕЛАКС
+                   ЕООД, <a href="tel:0888241248">0888241248</a></p>
+           </div>
+        </div>
+        <google-map :center="{ lat: 42.669065, lng: 23.291006 }"
+                    :marker="{ lat: 42.669065, lng: 23.291006 }"></google-map>
     </div>
 </template>
 
-<string></string>
+<script>
+import GoogleMap from '../components/Map.vue';
+
+export default {
+  name: 'project',
+  components: {
+    GoogleMap,
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+    .heading
+        color: #9C806B;
+        font-weight bold
+</style>
