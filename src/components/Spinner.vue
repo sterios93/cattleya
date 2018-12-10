9617b374170eab4eb659cb27a39a7d0758cf6aa3
stery <sterios.taskudis@gmail.com>
2018-12-10 02:05:45 +0200

    feat: Create Spinner component, and install it lib.

---
+++ src/components/Spinner.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="vld-parent">
+        <loading :active.sync="isLoading"
+        :color="'#9C806B'"
+        :can-cancel="true"
+        :on-cancel="onCancel"
+        :is-full-page="fullPage"></loading>
+    </div>
+</template>
+
+<script>
+// Import component
+import Loading from 'vue-loading-overlay';
+// Import stylesheet
+import 'vue-loading-overlay/dist/vue-loading.css';
+
+export default {
+  data() {
+    return {
+      isLoading: false,
+      fullPage: true,
+    };
+  },
+  components: {
+    Loading,
+  },
+  methods: {
+    doAjax() {
+      this.isLoading = true;
+      // simulate AJAX
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 500);
+    },
+    onCancel() {
+      console.log('User cancelled the loader.');
+    },
+  },
+  mounted() {
+    this.doAjax();
+  },
+};
+</script>
