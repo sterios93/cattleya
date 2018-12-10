e9f7d85c387b1ced1093cad8c5aef042e39dbbab
stery <sterios.taskudis@gmail.com>
2018-12-10 02:07:37 +0200

    fix: Update Project view layout.

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,8 +1,79 @@
 <template>
-    <div class="mt-5">
-        <div class="mt-0 mt-sm-5"></div>
-        <p>Project page mock</p>
-    </div>
+    <section class="container mt-lg-5">
+            <Spinner></Spinner>
+            <div class="mt-5">
+                <div class="mt-0 mt-sm-5 pt-lg-5"></div>
+                <Article v-bind:pictureLeft="false"
+                         :article="projectInfo.introduction" :isHome="false"></Article>
+                <div class="container m-0">
+                    <div class="row mt-5">
+                          <div class="flex-column col-lg-6">
+                            <p class="font-weight-bold">Сградата се състой от:</p>
+                            <div class="d-flex row">
+                                <ul class="col-12 col-md-6 flex-column">
+                                    <li>{{projectInfo.consist[0]}}</li>
+                                    <li>{{projectInfo.consist[1]}}</li>
+                                    <li>{{projectInfo.consist[2]}}</li>
+                                    <li>{{projectInfo.consist[3]}}</li>
+                                </ul>
+                                <ul class="col-12 col-md-6 flex-column">
+                                    <li>{{projectInfo.consist[4]}}</li>
+                                    <li>{{projectInfo.consist[5]}}</li>
+                                    <li>{{projectInfo.consist[6]}}</li>
+                                </ul>
+                            </div>
+                          </div>
+                         <div class="flex-column col-lg-6">
+                            <p class="font-weight-bold">Локация на сградата:</p>
+                            <div class="d-flex row">
+                                <ul class="col-12 col-md-6 flex-column">
+                                    <li>{{projectInfo.location[0]}}</li>
+                                    <li>{{projectInfo.location[1]}}</li>
+                                    <li>{{projectInfo.location[2]}}</li>
+                                    <li>{{projectInfo.location[3]}}</li>
+                                </ul>
+                                <ul class="col-12 col-md-6 flex-column">
+                                    <li>{{projectInfo.location[4]}}</li>
+                                    <li>{{projectInfo.location[5]}}</li>
+                                </ul>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+                <Article v-bind:pictureLeft="false"
+                         :article="projectInfo.environment" :isHome="false"></Article>
+                <Article v-bind:pictureLeft="true"
+                         :article="projectInfo.architectural" :isHome="false"></Article>
+                <google-map></google-map>
+            </div>
+    </section>
 </template>
 
-<string></string>
+<script>
+import { mapGetters } from 'vuex';
+import Article from '../components/Article.vue';
+import GoogleMap from '../components/Map.vue';
+import Spinner from '../components/Spinner.vue';
+
+export default {
+  name: 'project',
+  components: {
+    Article,
+    GoogleMap,
+    Spinner,
+  },
+  data() {
+    return {
+      projectInfo: {},
+    };
+  },
+  computed: {
+    ...mapGetters([
+      'getProjectDescription',
+    ]),
+  },
+  beforeMount() {
+    this.projectInfo = this.getProjectDescription;
+  },
+};
+</script>
