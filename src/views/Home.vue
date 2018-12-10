fc5cdd8933fdba3d44566c74379c5f1581531c4c
stery <sterios.taskudis@gmail.com>
2018-12-10 02:09:18 +0200

    fix: update Home view.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="home">
+    <Spinner></Spinner>
     <Header></Header>
     <main>
-      <Article v-bind:pictureLeft="true"></Article>
-      <Article v-bind:pictureLeft="false"></Article>
-      <Article v-bind:pictureLeft="true"></Article>
+      <Article
+            v-bind:pictureLeft="true"
+            :article="about.team"
+            :isHome="true"
+            :to="about.team.route">
+      </Article>
+      <Article
+            v-bind:pictureLeft="false"
+            :article="projectInfo.environment"
+            :isHome="true"
+            :to="projectInfo.environment.route">
+      </Article>
+      <Article
+            v-bind:pictureLeft="true"
+            :article="projectInfo.introduction"
+            :isHome="true"
+            :to="projectInfo.environment.route">
+      </Article>
       <google-map></google-map>
     </main>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import Header from '../components/Header.vue';
 import Article from '../components/Article.vue';
 import GoogleMap from '../components/Map.vue';
+import Spinner from '../components/Spinner.vue';
 
 export default {
   name: 'home',
@@ -21,6 +39,23 @@ export default {
     Header,
     Article,
     GoogleMap,
+    Spinner,
+  },
+  data() {
+    return {
+      projectInfo: {},
+      about: {},
+    };
+  },
+  computed: {
+    ...mapGetters([
+      'getProjectDescription',
+      'getAbout',
+    ]),
+  },
+  beforeMount() {
+    this.projectInfo = this.getProjectDescription;
+    this.about = this.getAbout;
   },
 };
 </script>
