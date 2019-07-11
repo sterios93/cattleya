4d164f5ed70d8371ec1c686452b66bd2c8821de4
sterios <sterios.taskudis@gmail.com>
2019-07-11 23:16:02 +0300

    fix: remove resize event on beforeDestroy

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,24 +1,25 @@
-<!--Todos-->
-<!--Fix the responsive layout-->
-<!--make the content dynamic-->
-
 <template>
     <section class="d-flex mt-5 container">
         <div class="row">
-            <div class="col-12 col-sm-12 col-lg-7 d-flex justify-content-center"
+            <div class="col-12 col-lg-7 d-flex justify-content-center"
                  v-bind:class="['order-2', { 'order-1' : picturePosition }]">
-                <img class="mw-100 " :src="require(`../assets/${article.imageSrc}`)"
-                     style="object-fit:cover;" alt="some photo change it later">
+                <img 
+                  class="mw-100 " 
+                  :src="require(`../assets/${article.imageSrc}`)"
+                  style="object-fit:cover;" 
+                  alt="some photo change it later"
+                  >
             </div>
-            <div class="col-12 col-sm-12 col-lg-5 d-flex mt-3 mt-lg-0
-             flex-column justify-content-center"
+            <div class="col-12 col-lg-5 d-flex mt-3 mt-lg-0 flex-column justify-content-center"
                  v-bind:class="[ 'order-1', { 'order-2' : picturePosition }]">
                 <h1 class="heading text-left">{{article.heading}}</h1>
-                <p >
-                  {{article.description}}
-                </p>
-                  <router-link :to="to" v-if="isHome !==false"
-                               class="blackText">Вижте повече</router-link>
+                <p>{{article.description}} </p>
+                  <router-link 
+                    :to="to"
+                    v-if="isHome !==false"
+                    class="heading">
+                    Вижте повече
+                  </router-link>
             </div>
         </div>
     </section>
@@ -32,21 +33,22 @@ export default {
       picturePosition: null,
     };
   },
+  created() {
+    this.changePicOrder();
+    this.attachListeners();
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.changePicOrder);
+  },
   methods: {
     attachListeners() {
-      window.addEventListener('resize', () => {
-        this.changePicOrder();
-      });
+     window.addEventListener('resize', this.changePicOrder);
     },
     changePicOrder() {
       const windowWidth = window.innerWidth;
       if (windowWidth < 992) this.picturePosition = true;
       else this.picturePosition = this.pictureLeft;
     },
-  },
-  created() {
-    this.changePicOrder();
-    this.attachListeners();
   },
 };
 </script>
